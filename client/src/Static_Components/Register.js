c745f1145662801dcf63390999869190566f328b
amitaysoffer <amitaysof@gmail.com>
2020-11-18 23:15:43 +0000

    register modal updated

--- client/src/Static_Components/Register.js
+++ client/src/Static_Components/Register.js
@@ -8,8 +8,32 @@ function Register(props) {
     return (
       <div id="myModal" className="modal">
         <div className="modal-content">
-          <span className="close" onClick={props.toggleModal}>&times;</span>
-          <p>Some text in the Modal..</p>
+          <form>
+
+            <div class="container">
+
+              <span className="close" onClick={props.toggleModal}>&times;</span>
+
+              <label htmlFor="email"><b>Email</b></label>
+              <input type="text" placeholder="Enter Email" name="email" required />
+
+              <label htmlFor="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required />
+
+              <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
+              <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+
+              <label>
+                <input type="checkbox" checked="checked" name="remember" defaultChecked /> Remember me</label>
+
+              <p>By creating an account you agree to our <a >Terms & Privacy</a>.</p>
+
+              <div class="clearfix">
+                <button type="button" class="cancelbtn">Cancel</button>
+                <button type="submit" class="signupbtn">Sign Up</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
 
