21da2249599bc4eefee3e78c07220e074fd160e1
amitaysoffer <amitaysof@gmail.com>
2020-12-11 17:51:28 +0000

    updated functions

--- client/src/Page_Components/trade/Form.js
+++ client/src/Page_Components/trade/Form.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
-import { useEffect } from 'react';
+import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi'
 
 const Form = (props) => {
   const [symbol, setSymbol] = useState('');
   const [companyName, setCompanyName] = useState(null);
+  const [isCanSearch, setCanSearch] = useState(true);
+  const timeoutRef = useRef(null);
 
   function handleSubmit(e) {
     e.preventDefault()
     props.searchForHolding(symbol)
   }
+
+  const onSearchSymbol = (e) => {
+    setCanSearch(prevState => prevState = false);
+    setSymbol(e.target.value);
+  }
+
+  useEffect(() => {
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    if (isCanSearch) performApiCall();
+
+    timeoutRef.current = setTimeout(() => {
+      timeoutRef.current = null;
+      setCanSearch(true);
+    }, 1000);
+  }, [isCanSearch]);
 
   const performApiCall = async () => {
     try {
@@ -21,59 +41,18 @@ const Form = (props) => {
     }
   }
 
-  useEffect(() => {
-    performApiCall()
-  }, [symbol]);
-
-
-
-  // const [symbol, setSymbol] = useState('');
-  // const [shares, setShare] = useState('');
-  // const [companyName, setCompanyName] = useState('');
-
-  // const [isCanSearch, setCanSearch] = useState(true);
-  // const timeoutRef = useRef(null);
-
-  // function onSearchSymbol(e) {
-  //   setCanSearch(prevState => prevState = false);
-  //   setSymbol(e.target.symbol);
-  // }
-
-  // useEffect(() => {
-  //   if (timeoutRef.current !== null) {
-  //     clearTimeout(timeoutRef.current);
-  //   }
-
-  //   if (isCanSearch) performApiCall();
-
-  //   timeoutRef.current = setTimeout(() => {
-  //     timeoutRef.current = null;
-  //     setCanSearch(true);
-  //   }, 1000);
-  // }, [isCanSearch]);
-
-
-  // const performApiCall = async () => {
-  //   try {
-  //     const response = await axios.get(`api/stocks/search/?symbol=${symbol}`);
-  //     setCompanyName(response.data.companyName);
-  //     console.log(response.data.companyName)
-  //   } catch (err) {
-  //     console.error(err.message)
-  //   }
-  // }
-
   return (
     <form className="form-inline justify-content-center mt-3" onSubmit={handleSubmit}>
       <input
         type="symbol"
         value={symbol}
-        onChange={(e) => setSymbol(e.target.value)}
+        onChange={(e) => onSearchSymbol(e)}
         className="form-control col-sm-5 text-uppercase"
       />
       <button type="submit" className="btn btn-primary col-sm-2">Submit</button>
-      <p className="position-absolute">{companyName ? companyName : ''}</p>
-    </form>
+      {/* {companyName ? <FiThumbsUp /> : <FiThumbsDown />} */}
+      <h6 className="w-100 text-center text-success" style={{ display: symbol === '' ? 'none' : 'block' }}>{companyName}</h6>
+    </form >
   )
 }
 
