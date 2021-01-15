a5f2b6719b529cf8ff84f4b9ee4b3a6fb9934fe0
amitaysoffer <amitaysof@gmail.com>
2021-01-15 23:45:02 +0100

    fixed db script

--- database/db.js
+++ database/db.js
@@ -1,13 +1,5 @@
 const Pool = require('pg').Pool
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   password: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'tradingstocks'
-// });
-
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
