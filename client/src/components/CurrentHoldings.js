85c883d7d935f2c2253ed42182b2c03108f1667e
amitaysoffer <amitaysof@gmail.com>
2021-01-16 18:38:39 +0100

    added nocurrentholdings compoennt

--- client/src/components/CurrentHoldings.js
+++ client/src/components/CurrentHoldings.js
@@ -21,58 +21,57 @@ const CurrentHoldings = (props) => {
 
   const { name, symbol, price, percent_change, shares } = props.holding;
 
-  if (props.holding) {
+  // if (props.holding) {
 
-
-    return (
-      <div className="selected-holding card mt-4">
-        <div className="card-head">
-          <h2> {name}: {symbol}</h2>
-          <div className="card-buttons">
-            <Link to={{
-              pathname: "/main",
-              search: `?symbol=${symbol}`
-            }}>
-              <button className="btn btn-primary">Trade</button>
-            </Link>
-          </div>
-        </div>
-        <hr />
-        <div className="card-body">
-          <div className="Holding Value:">
-            <strong>Holding Value</strong>
-            <p className={holdingStyleColor}>${Number(price * shares).toFixed(2)}</p>
-          </div>
-          <div className="shares">
-            <strong>Shares:</strong>
-            <p>{shares}</p>
-          </div>
-          <div className="last-price">
-            <strong>Last Price:</strong>
-            <p className={holdingStyleColor}>${price}</p>
-          </div>
-          <div className="percent-change">
-            <strong>Percent Change:</strong>
-            <p className={holdingStyleColor}>{positiveSign}{percent_change}%</p>
-          </div>
-        </div>
-      </div >
-    )
-  } else {
-    return (
-      <Card className="text-center mt-5">
-        <Card.Body className="d-block border">
-          <Card.Title>You don't own any holdings as of yet.</Card.Title>
-          <Card.Text>
-            Go to our Trade page to start purchasing new stocks.
-    </Card.Text>
-          <Link to="/main">
-            <Button variant="primary">Trade Page</Button>
+  return (
+    <div className="selected-holding card mt-4">
+      <div className="card-head">
+        <h2> {name}: {symbol}</h2>
+        <div className="card-buttons">
+          <Link to={{
+            pathname: "/main",
+            search: `?symbol=${symbol}`
+          }}>
+            <button className="btn btn-primary">Trade</button>
           </Link>
-        </Card.Body>
-      </Card>
-    )
-  }
+        </div>
+      </div>
+      <hr />
+      <div className="card-body">
+        <div className="Holding Value:">
+          <strong>Holding Value</strong>
+          <p className={holdingStyleColor}>${Number(price * shares).toFixed(2)}</p>
+        </div>
+        <div className="shares">
+          <strong>Shares:</strong>
+          <p>{shares}</p>
+        </div>
+        <div className="last-price">
+          <strong>Last Price:</strong>
+          <p className={holdingStyleColor}>${price}</p>
+        </div>
+        <div className="percent-change">
+          <strong>Percent Change:</strong>
+          <p className={holdingStyleColor}>{positiveSign}{percent_change}%</p>
+        </div>
+      </div>
+    </div >
+  )
+  // } else {
+  //   return (
+  //     <Card className="text-center mt-5">
+  //       <Card.Body className="d-block border">
+  //         <Card.Title>You don't own any holdings as of yet.</Card.Title>
+  //         <Card.Text>
+  //           Go to our Trade page to start purchasing new stocks.
+  //   </Card.Text>
+  //         <Link to="/main">
+  //           <Button variant="primary">Trade Page</Button>
+  //         </Link>
+  //       </Card.Body>
+  //     </Card>
+  //   )
+  // }
 }
 
 export default CurrentHoldings
