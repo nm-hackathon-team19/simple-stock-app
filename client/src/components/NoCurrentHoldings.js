aaf154f7f6a554eda9b77cc38fea2c3abd413fa9
amitaysoffer <amitaysof@gmail.com>
2021-01-16 19:34:24 +0100

    updated no currentholdings

--- client/src/components/NoCurrentHoldings.js
+++ client/src/components/NoCurrentHoldings.js
@@ -7,9 +7,7 @@ const NoCurrentHoldings = () => {
     <Card className="text-center mt-5">
       <Card.Body className="d-block border">
         <Card.Title>You don't own any holdings as of yet.</Card.Title>
-        <Card.Text>
-          Go to our Trade page to start purchasing new stocks.
-    </Card.Text>
+        <Card.Text>Go to our Trade page to start purchasing new stocks. </Card.Text>
         <Link to="/main">
           <Button variant="primary">Trade Page</Button>
         </Link>
