893c8bfec6f7a1ba8146e2d3022361b535a8dc48
amitaysoffer <amitaysof@gmail.com>
2021-01-22 11:58:20 +0000

    progress of home page fix

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
 import { UserNameContext } from '../context/UserNameContext'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Row, Col, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { GiReceiveMoney } from 'react-icons/gi'
+import { AiOutlineDatabase } from 'react-icons/ai'
+import { BiBookReader } from 'react-icons/bi'
 
 const WelcomePage = () => {
   const [userName, setUserName] = useContext(UserNameContext)
@@ -10,27 +13,52 @@ const WelcomePage = () => {
     <div id="home">
       <div className="home-content m-0 text-justify w-100">
         <h1 className="text-center">Hi {userName}, and welcome to UK's No. 1 trading playground!</h1>
-        <Card bg="transparent" border="primary" className="w-75 mt-3">
-          <Card.Body className="d-block">
-            <Card.Title as="h3">Trading Page</Card.Title>
-            <Card.Text>
-              Buy stocks by searching for a specific stock, or view recommended stocks. You can also buy or sell stocks that you already have in your portfolio. You are initially given a total of 10,000 dollars to buy stocks with.</Card.Text>
-            <Link to="/main">
-              <Button variant="primary">Start Trading</Button>
-            </Link>
-          </Card.Body>
-        </Card>
-        <Card bg="transparent" border="primary" className="mt-3 w-75">
-          <Card.Body className="d-block">
-            <Card.Title as="h3">Portfolio Page</Card.Title>
-            <Card.Text>
-              To view your personal data and current holdings take a look at the portfolio page. With this information you can make informed decisions on what stocks to buy and sell.
+        <Container>
+          <Row>
+            <Col md={4}>
+              <Card bg="transparent" className="home-card mt-3">
+                <GiReceiveMoney size="7em" className="col-logo" />
+                <Card.Body className="d-block">
+                  <Card.Title as="h3" className="text-center">Trade</Card.Title>
+                  <Card.Text>
+                    Buy stocks by searching for a specific symbol, or view our recommended stocks. You are initially given a total of 10,000 dollars.</Card.Text>
+                  <Link to="/main">
+                    <Button variant="primary">Start Trading</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card bg="transparent" className="home-card mt-3">
+                <AiOutlineDatabase size="7em" className="col-logo" />
+                <Card.Body className="d-block">
+                  <Card.Title as="h3" className="text-center">Data</Card.Title>
+                  <Card.Text>
+                    View your personal data, current holdings and statistics.
+                    With this information you can make informed decisions on what stocks to buy and sell.
               </Card.Text>
-            <Link to="/portfolio">
-              <Button variant="primary">Personal Information</Button>
-            </Link>
-          </Card.Body>
-        </Card>
+                  <Link to="/portfolio">
+                    <Button variant="primary">Personal Information</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card bg="transparent" className="home-card mt-3">
+                <BiBookReader size="7em" className="col-logo" />
+                <Card.Body className="d-block">
+                  <Card.Title as="h3" className="text-center">Instructions</Card.Title>
+                  <Card.Text>
+                    New to the stocks world ? Want to get a better understanding of how to utilize the Fantasy Trade App ? This is the place for you.
+              </Card.Text>
+                  <Link to="/instructions">
+                    <Button variant="danger">Start Learning</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
