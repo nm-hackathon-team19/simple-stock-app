aedc3ce5d16e65283970cb4963780fe85ceca41d
amitaysoffer <amitaysof@gmail.com>
2021-01-22 00:39:20 +0000

    removed links

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './style/index.css';
 import './style/home.css'
 import './style/mobile.css'
