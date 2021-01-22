37ddddc7a158f45408b5b9da32ce00cf0c5be4ac
amitaysoffer <amitaysof@gmail.com>
2021-01-22 00:58:59 +0000

    changed text hero

--- client/src/components/BeginnersHero.js
+++ client/src/components/BeginnersHero.js
@@ -6,7 +6,7 @@ const BeginnersHero = () => {
       <div className="guide-text container">
         <h1 className="display-3">Fantazy Stocks Guide</h1>
         <h3>Want to learn how to trade? You're in the right place.</h3>
-        <h3>Scroll down to view some basic concepts and our top 10 active stocks at a given moment. </h3>
+        <h3><span className="text-success font-italic">Scroll down</span> to view some basic concepts and our top 10 active stocks at a given moment. </h3>
       </div>
     </section>
   )
