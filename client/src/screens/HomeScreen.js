863279987404c67eb06d17e6080ea598e39341c6
amitaysoffer <amitaysof@gmail.com>
2021-01-22 16:21:47 +0000

    app ready for review

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -7,14 +7,16 @@ const HomeScreen = () => {
   if (!localStorage.getItem('data')) {
     return (
       <div id="home">
-        <div className="home-content mt-5 h2 font-weight-light">
-          <h1 className="display-4 mb-5 home-header">Welcome to Fantasy Stocks Trading!</h1>
-          <p>Experience the thrill of the stock market by utilizing fake money to purchase real stocks and buy or sell shares.
-          Search stock symbols and use fake money to trade and test your skills.</p>
-          <p>Before you start, you must first register so the system can identify who you are.</p>
-          <div className="home-buttons-wrapper">
-            <Register />
-            <Login />
+        <h1 className="text-center display-4 text-light">Welcome to Fantazy Stocks</h1>
+        <div className="home-container">
+          <div className="home-content mt-5 h2 font-weight-light">
+            <h1 className="display-4 mb-5 home-header">The best choices... begin with practice.</h1>
+            <p>Experience the thrill of the stock market by utilizing fake money to purchase real stocks and buy or sell shares.</p>
+            <p>Before you start, you must first register.</p>
+            <div className="home-buttons-wrapper">
+              <Register />
+              <Login />
+            </div>
           </div>
         </div>
       </div>
