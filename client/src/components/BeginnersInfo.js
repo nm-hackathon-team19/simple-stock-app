22073ae88e418b74c51749aeb7b3bdc8711db0dd
amitaysoffer <amitaysof@gmail.com>
2021-01-29 10:53:38 +0000

    syntax reorg

--- client/src/components/BeginnersInfo.js
+++ client/src/components/BeginnersInfo.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import { Jumbotron, ListGroup } from 'react-bootstrap'
-import { BiRightArrow } from 'react-icons/bi'
+import React from 'react';
+import { Jumbotron, ListGroup } from 'react-bootstrap';
+import { BiRightArrow } from 'react-icons/bi';
 
 const BeginnersInfo = () => {
   return (
-    <Jumbotron as="section" className="guide-rules-container bg-transparent container mb-0 p-3">
+    <Jumbotron
+      as="section"
+      className="guide-rules-container bg-transparent container mb-0 p-3"
+    >
       <ListGroup variant="flush">
-        <ListGroup.Item><BiRightArrow className="text-success" /> <strong>Stock symbol</strong> is an abbreviation used to uniquely identify publicly traded shares of a particular stock. For example, FB is the symbol of Facebook. To search for stocks, visit our Trading page and enter the symbol you wish to search.</ListGroup.Item>
-        <ListGroup.Item><BiRightArrow className="text-success" /> <strong>Percentage change</strong> represents a degree of change over time. If the values are <span className="text-danger">negative and red</span>, that means that the price decreased. If they are <span className="text-success">positive and green</span> the price increased.
+        <ListGroup.Item>
+          <BiRightArrow className="text-success" />{' '}
+          <strong>Stock symbol</strong> is an abbreviation used to uniquely
+          identify publicly traded shares of a particular stock. For example, FB
+          is the symbol of Facebook. To search for stocks, visit our Trading
+          page and enter the symbol you wish to search.
         </ListGroup.Item>
-        <ListGroup.Item><BiRightArrow className="text-success" /> <strong>A stock price</strong> is given for every share issued by a publicly traded company. The price is a reflection of the company's value – what the public is willing to pay for a piece of the company.
-    </ListGroup.Item>
+        <ListGroup.Item>
+          <BiRightArrow className="text-success" />{' '}
+          <strong>Percentage change</strong> represents a degree of change over
+          time. If the values are{' '}
+          <span className="text-danger">negative and red</span>, that means that
+          the price decreased. If they are{' '}
+          <span className="text-success">positive and green</span> the price
+          increased.
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <BiRightArrow className="text-success" />{' '}
+          <strong>A stock price</strong> is given for every share issued by a
+          publicly traded company. The price is a reflection of the company's
+          value – what the public is willing to pay for a piece of the company.
+        </ListGroup.Item>
       </ListGroup>
     </Jumbotron>
-  )
-}
+  );
+};
 
-export default BeginnersInfo
+export default BeginnersInfo;
--- client/src/components/MostActiveStocks.js
+++ client/src/components/MostActiveStocks.js
@@ -1,7 +1,7 @@
-import React, { useState, Fragment, useEffect } from 'react'
-import axios from 'axios'
-import RenderMostActiveStocks from './RenderMostActiveStocks'
-import { Spinner } from 'react-bootstrap'
+import React, { useState, Fragment, useEffect } from 'react';
+import axios from 'axios';
+import RenderMostActiveStocks from './RenderMostActiveStocks';
+import { Spinner } from 'react-bootstrap';
 
 const MostActiveStocks = () => {
   const [mostActiveStocks, setMostActiveStocks] = useState([]);
@@ -9,36 +9,36 @@ const MostActiveStocks = () => {
 
   useEffect(() => {
     const getRecommendations = () => {
-      axios.get('/api/stocks/mostactive')
-        .then((res) => {
-          setMostActiveStocks(res.data)
-          setSpinner(false)
+      axios
+        .get('/api/stocks/mostactive')
+        .then(res => {
+          setMostActiveStocks(res.data);
+          setSpinner(false);
         })
-        .catch((err) => {
-          console.log("error mostactive stocks client side", err);
+        .catch(err => {
+          console.log('error mostactive stocks client side', err);
         });
-    }
+    };
 
     getRecommendations();
-  }, [])
+  }, []);
 
   return (
     <section className="container-fluid active-stocks-container">
       <div className="most-active-list d-flex flex-wrap justify-content-center">
-        {
-          isSpinner ?
-            <Spinner animation="border" className="spinner" />
-            :
-            mostActiveStocks.map(mostActiveStock => (
-              <RenderMostActiveStocks
-                mostActiveStock={mostActiveStock}
-                key={mostActiveStock.marketCap}
-              />
-            ))
-        }
+        {isSpinner ? (
+          <Spinner animation="border" className="spinner" />
+        ) : (
+          mostActiveStocks.map(mostActiveStock => (
+            <RenderMostActiveStocks
+              mostActiveStock={mostActiveStock}
+              key={mostActiveStock.marketCap}
+            />
+          ))
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MostActiveStocks
+export default MostActiveStocks;
