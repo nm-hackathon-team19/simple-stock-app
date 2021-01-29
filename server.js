6a7e5caa3066ba60af5ccbc73143df4e85a46362
amitaysoffer <amitaysof@gmail.com>
2021-01-29 10:48:27 +0000

    reorg syntax server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
 const app = express();
 const path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'client/build'))); // Set build folder
 
-app.use(express.json()) // access req.body 
+app.use(express.json()); // access req.body
 
 const tradeRoutes = require('./server-routes/tradeRoutes');
 app.use('/trade/', tradeRoutes);
@@ -20,7 +20,9 @@ const apiRoutes = require('./server-routes/apiRoutes');
 app.use('/api/', apiRoutes);
 
 // Catch all
-app.get("/*", (req, res) => { res.sendFile(path.join(__dirname, "client", "build", "index.html")); });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
