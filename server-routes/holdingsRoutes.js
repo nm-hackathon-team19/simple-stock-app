021c057aa0de33dd36f9389d4085e8ad6f7ef9f2
amitaysoffer <amitaysof@gmail.com>
2021-02-08 14:25:48 +0000

    added uuid to username table

--- server-routes/holdingsRoutes.js
+++ server-routes/holdingsRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database/db');
 
-// get holdings
 router.get('/:user_id', async (req, res) => {
   try {
     const { user_id } = req.params;
